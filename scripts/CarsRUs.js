414cef910df30aa5999d69f272efb6ee162496ff
Jeremy Myers <echelonical@gmail.com>
2022-08-16 14:24:51 -0500

    added modules for paintColor, Interior, Technolog, and Wheels

---
+++ scripts/CarsRUs.js
@@ -0,0 +1,41 @@
+
+
+
+document.addEventListener(
+    "click",
+    (event) => {
+        if (event.target.id.startsWith("orderButton")) //add click event to target button at the id specified
+            addCustomOrder() //call function to print permanent changes to page.
+    }
+)
+
+export const CarsRUs = () => {
+    return `
+        <h1>Kneel Diamonds</h1>
+
+        <article class="choices">
+            <section class="choices__metals options">
+                <h2>Metals</h2>
+                ${Metals()}
+            </section>
+            <section class="choices__sizes options">
+                <h2>Sizes</h2>
+                ${DiamondSizes()}
+            </section>
+            <section class="choices__styles options">
+                <h2>Styles</h2>
+                ${JewelryStyles()}
+            </section>
+        </article>
+
+        <article>
+            <button id="orderButton">Create Custom Order</button>
+        </article>
+
+        <article class="customOrders">
+            <h2>Custom Jewelry Orders</h2>
+            ${Orders()}
+        </article>
+    `
+}
+
